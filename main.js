3762d912383e1cb343253286daca72d5321c3f30
Fumiaki Takizawa <tkzwfm21@gmail.com>
2019-10-05 12:42:59 +0900

    go pull

--- main.js
+++ main.js
@@ -2,13 +2,13 @@
 //   - コメントとは: https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Lexical_grammar#%E3%82%B3%E3%83%A1%E3%83%B3%E3%83%88
 
 // ここに「ブロックスコープ」の説明を記述する
-
+// {}で囲まれた内部で宣言された変数（const, let）は｛｝外では利用できない。varはどこでも使える
 
 // 課題2: 「コメント」を使って変数の関数スコープの説明をしてください
 //   - コメントとは: https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Lexical_grammar#%E3%82%B3%E3%83%A1%E3%83%B3%E3%83%88
 
 // ここに「関数スコープ」の説明を記述する
-
+// 関数内で宣言された変数は関数内でのみ利用できる。（var, let, const）
 
 // 課題3: 以下の条件を満たす高階関数を実装してください
 //   - 関数名: kadai_3
@@ -25,7 +25,28 @@
 //         - 処理内容:
 //           - 第1引数で受けとった `message` の内容を `alert` を使ってアラートダイアログに表示する
 //   - kadai_3関数を実装した直後に「kadai_3(数値, コールバック関数)」を呼び出して、アラートダイアログのメッセージ内容が意図通りであることを確認する
+function kadai_3(age, callback) {
+    let message;
+    if (typeof age !== 'number') {
+        message = '数値が入力されていません';
+    } else if (age >= 20) {
+        message = '値は20以上です';
+    } else if (age >= 10 && age < 20) {
+        message = '値は10以上20未満です';
+    } else if (age < 10) {
+        message = '値は10未満です';
+    };
 
+    callback(message);
+};
+
+function alm(value) {
+    alert(value);
+};
+
+kadai_3(9, alm);
+kadai_3(10, alm);
+kadai_3(20, alm);
 
 // 課題4: 以下の条件を満たす即時関数を作る
 //   - 2つの引数を受け取る
@@ -33,3 +54,23 @@
 //     - 第2引数: y => 数値
 //   - 処理内容:
 //     - 第1引数のx, 第2引数のyを使って足し算した結果(「x + y」の結果)をconsole.logで出力する。
+((x, y) => {
+    console.log(x + y);
+})(10, 20);
+
+
+// function timeMesure(func){
+//     const startTime = new Date();
+//     func();
+//     const endTime = new Date();
+//     const diff = endTime - startTime;
+//     return diff;
+// };
+
+// let result = timeMesure(() => {
+//     for(let i = 0; i <= 100000000; i++){
+//         ;
+//     };
+// });
+
+// console.log(result);
